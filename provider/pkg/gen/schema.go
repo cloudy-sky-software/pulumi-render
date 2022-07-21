e5d175b40a65046c04b78119f257d1937983c067
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2022-07-20 21:41:38 -0700

    Set the PluginDownloadURL in the schema package

--- provider/pkg/gen/schema.go
+++ provider/pkg/gen/schema.go
@@ -137,10 +137,14 @@ func PulumiSchema(openapiDoc openapi3.T) (pschema.PackageSpec, ProviderMetadata)
 			},
 		},
 
-		Types:     map[string]pschema.ComplexTypeSpec{},
-		Resources: map[string]pschema.ResourceSpec{},
-		Functions: map[string]pschema.FunctionSpec{},
-		Language:  map[string]pschema.RawMessage{},
+		// This URL should be the base releases URL for a version,
+		// to which the plugin name is suffixed.
+		// https://github.com/pulumi/pulumi/blob/c7e17febe5ff0396e25dca24f59ca98f6722edf2/sdk/go/common/workspace/plugins.go#L379
+		PluginDownloadURL: "https://github.com/cloudy-sky-software/pulumi-render/releases/download/v${VERSION}",
+		Types:             map[string]pschema.ComplexTypeSpec{},
+		Resources:         map[string]pschema.ResourceSpec{},
+		Functions:         map[string]pschema.FunctionSpec{},
+		Language:          map[string]pschema.RawMessage{},
 	}
 
 	csharpNamespaces := map[string]string{

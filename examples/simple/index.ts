81a386726c226d46ff1d231ea0b0b2c05bf82d47
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2023-01-14 11:44:56 -0800

    Update the example app

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -1,15 +1,67 @@
-import * as render from "@css/render";
+import * as pulumi from "@pulumi/pulumi";
+import * as render from "@cloudyskysoftware/pulumi-render";
 
-import * as o from "@css/render/types/output";
+import { services } from "@cloudyskysoftware/pulumi-render/types/input";
 
-const service = new render.services.Service("my-service", {
-  name: "My test service",
-  ownerId: "",
-  repo: "https://github.com/praneetloke/test",
-  type: "static_site",
-  serviceDetails: {},
+const ownerId = pulumi
+    .output(render.owners.listOwners())
+    .apply(
+        (result) =>
+            result.items.filter(
+                (i) => i.owner?.email === "pl@cloudysky.software"
+            )[0].owner?.id || ""
+    );
+
+const staticSiteDetails: services.StaticSiteServiceDetailsArgs = {
+    publishPath: "public",
+};
+
+const staticSite = new render.services.StaticSite("staticsite", {
+    name: "My custom static site",
+    ownerId,
+    repo: "https://github.com/cloudy-sky-software/test-static-site",
+    autoDeploy: "no",
+    branch: "main",
+    serviceDetails: staticSiteDetails,
 });
 
-export const staticSiteUrl = service.serviceDetails.apply(
-  (d) => (d as o.services.ServiceStaticSite).url
-);
+const port = 8080;
+
+const webServiceDetails: services.WebServiceServiceDetailsArgs = {
+    env: "node",
+    plan: "starter",
+    region: "oregon",
+    openPorts: [
+        {
+            port,
+            protocol: "TCP",
+        },
+    ],
+    envSpecificDetails: {
+        buildCommand: "yarn",
+        startCommand: "node app.js",
+    },
+};
+
+const webService = new render.services.WebService("webservice", {
+    name: "An Express.js web service",
+    ownerId,
+    repo: "https://github.com/render-examples/express-hello-world",
+    autoDeploy: "yes",
+    envVars: [
+        {
+            key: "PORT",
+            value: `${port}`,
+        },
+    ],
+    branch: "master",
+    serviceDetails: webServiceDetails,
+});
+
+export const url = (
+    staticSite.serviceDetails as services.StaticSiteServiceDetailsArgs
+).url;
+
+export const webServiceUrl = (
+    webService.serviceDetails as services.WebServiceServiceDetailsArgs
+).url;

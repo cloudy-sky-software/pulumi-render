61ff9b990e3d3b71dad8738ac2f0e24a94cebcc0
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2024-05-04 16:38:39 -0700

    Regen schema and SDKs

--- sdk/dotnet/Registrycredentials/ListRegistrycredentials.cs
+++ sdk/dotnet/Registrycredentials/ListRegistrycredentials.cs
@@ -7,7 +7,7 @@ using System.Collections.Immutable;
 using System.Threading.Tasks;
 using Pulumi.Serialization;
 
-namespace Pulumi.Render.Registrycredentials
+namespace Pulumi.Render.RegistryCredentials
 {
     public static class ListRegistryCredentials
     {
--- sdk/dotnet/Registrycredentials/Outputs/GetRegistrycredentialProperties.cs
+++ sdk/dotnet/Registrycredentials/Outputs/GetRegistrycredentialProperties.cs
@@ -7,7 +7,7 @@ using System.Collections.Immutable;
 using System.Threading.Tasks;
 using Pulumi.Serialization;
 
-namespace Pulumi.Render.Registrycredentials.Outputs
+namespace Pulumi.Render.RegistryCredentials.Outputs
 {
 
     [OutputType]

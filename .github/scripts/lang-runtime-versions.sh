1f14dc890adaf83b3f7f50caf516ec101b685441
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2025-09-20 11:08:10 -0700

    Use a single script to source versions for language runtimes

---
+++ .github/scripts/lang-runtime-versions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "DOTNET_VERSION=8.0.x" >> "$GITHUB_ENV"
+echo "GO_VERSION=1.24.x" >> "$GITHUB_ENV"
+echo "NODE_VERSION=22.x" >> "$GITHUB_ENV"
+echo "PYTHON_VERSION=3.13" >> "$GITHUB_ENV"
